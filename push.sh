136300120ee56138ce6508f50d00fc982dac8ae2
codepen24 <codepen24@protonmail.com>
2023-02-01 11:45:15 +0800

    save shell modification

---
+++ push.sh
@@ -0,0 +1,5 @@
+while true; do
+    export LANG=en_US.UTF-8
+    rsync -avz --progress /home/cp_24/wamp64/www/devs/dilate/wp/kustomtimber/wp-content/themes/dilate-framework kustomtimber@kustomtimber.ddev.com.au:/home/kustomtimber/public_html/wp-content/themes/
+    sleep 10 
+done
