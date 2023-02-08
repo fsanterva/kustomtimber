dd0e1aacbbe6b09c19bc726f06f566a90ee28f40
codepen24 <codepen24@protonmail.com>
2023-02-08 10:58:18 +0800

    reset part 2

--- wp-content/pull.sh
+++
@@ -1,8 +0,0 @@
-run_flag=false
-while [ "$run_flag" = false ]; do
-  export LANG=en_US.UTF-8
-  rsync -avz --update --delete --progress kustomtimber@kustomtimber.ddev.com.au:/home/kustomtimber/public_html/wp-content/themes/dilate-framework/* /mnt/c/wamp64/www/devs/dilate/wp/kustomtimber/
-  git status .
-  sleep 2 
-  run_flag=false
-done
--- wp-content/themes/dilate-framework/components/related_projects/index.php
+++ wp-content/themes/dilate-framework/components/related_projects/index.php
@@ -35,15 +35,14 @@ require get_template_directory() . '/inc/component-wrapper-top.php';
   <?php if( $dataFeedAuto ) : 
     $currProjID = get_the_ID();
     $currRange = get_the_terms( $currProjID, 'range' );
-    $currFinish = get_field('finish', $currProjID);
   
     $tax_q = array('relation'=>'AND');
-    $meta_q = array('relation'=>'AND');
   
     $args = array(
       'post_type'       => 'project',
       'posts_per_page'  => 4,
-      'post_status '    => array('publish')
+      'post_status '    => array('publish'),
+      'orderby'         => 'rand',
     );
   
     if( !empty( $currRange ) ) {
@@ -54,32 +53,59 @@ require get_template_directory() . '/inc/component-wrapper-top.php';
       ));
     }
   
-    if( !empty($currFinish) ) {
-      array_push($meta_q, array(
-        'key' => 'finish',
-        'value' => $currFinish,
-      ));
-    }
-  
     $args['tax_query'] = $tax_q;
-    $args['meta_query'] = $meta_q;
   
     $result = new WP_Query( $args );
     $new_search = $result->posts;
   
     if( !empty($new_search) ) : ?>
-
+    <div class="project__wrapper">
       <?php foreach( $new_search as $obj ) : 
-        $pid = $obj->ID;
+        $pID = $obj->ID;
         $title = get_the_title($obj);
         $perm = get_the_permalink($obj);
+        $projRange = get_the_terms( $pID, 'range' );
+        $projRangeName = $currRange[0]->name;
+        $projfinish = get_field('finish', $obj);
+        $projPattern = get_field('pattern', $obj);
+        $featImg = getFeaturedImage($pID);
       ?>
 
-      <div>
-        <?=$perm?>
+      <div class="item">
+        
+        <div class="data__blocks">
+          
+          <div class="data__block data__block--name">
+            <label>Project Name</label>
+            <span class="value"><?= $title; ?></span>
+          </div>
+
+          <div class="data__block data__block--range">
+            <label>Collection</label>
+            <span class="value"><?= $projRangeName; ?></span>
+          </div>
+
+          <div class="data__block data__block--finish">
+            <label>Finish</label>
+            <span class="value"><?= get_the_title($projfinish); ?></span>
+          </div>
+
+          <div class="data__block data__block--pattern">
+            <label>Pattern</label>
+            <span class="value"><?= $projPattern['value']; ?></span>
+          </div>
+          
+        </div>
+        
+        <div class="featured__image">
+          <a href="<?= $perm; ?>" class="link-to-post" aria-name="Link to <?=$title?>"></a>
+          <img data-src="<?= $featImg['url']; ?>" alt="<?= $featImg['alt']; ?>"/>
+        </div>
+        
       </div>
     
       <?php endforeach; ?>
+  </div>
     <?php endif; ?>
   
   <?php endif; ?>
