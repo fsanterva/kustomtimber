bb5ceda0ca598b3ce3b5aee7b90eabe36e312799
fred <codepen24@protonmail.com>
2023-06-21 12:57:55 +0800

    save my stuffs

--- wp-content/themes/dilate-framework/functions.php
+++ wp-content/themes/dilate-framework/functions.php
@@ -15,87 +15,68 @@ require get_template_directory() . '/lib/register-taxonomies.php';
 // require get_template_directory() . '/lib/temp.php';
 
 
-function r_inspect($val) {
-    echo '<pre class="pre pre_red">';
-    print_r($val);
-    echo '</pre>';
-}
-
-function v_inspect($val) {
-    echo '<pre class="pre pre_red">';
-    var_dump($val);
-    echo '</pre>';
-}
-
-function get_what_page() {
-    global $post;
-    echo print_r('<span class="athena">' . get_page($post->id)->post_type . ' <a href="' . get_bloginfo("url") . '/wp-admin/post.php?post=' . get_the_ID($post->id)  . '&action=edit">wp-admin edit</a>' . ' ' . get_the_ID($post->id) ) . '</span>';
-} 
-add_action('wp_head', 'get_what_page' );
-
-
-
-
-
-
-function add_blog_posts() { // Insert Blog Post
-    $titles = array(
-        'Tips and Tricks To Make Your Timber Floors Look Like New',
-	'Getting The Most Out of Your Melbourne Hardwood Flooring Service',
-	'What You Need To Know About Melbourne Engineered Timber Flooring',
-	'Popular Timber Flooring Colours Perfect For Your Home Design',
-	'Why Most Australians Go For Timber Flooring Installations',
-	'Things to Consider Before You Install a Wood Floor',
-	'Timber Flooring: Different Species, Different Characteristics',
-	'The Complete Process of How To Sand Timber Floors',
-	'A Buyer’s Guide for Choosing The Best Wooden Flooring',
-	'Timber Stairs - Give me the facts',
-	'What is Blackbutt Flooring?',
-	'Why Choose Oak Flooring?',
-	'What Can You Expect From Timber Flooring Services Melbourne?',
-	'4 Signs That You Should Leave Your Floor Sanding to The Professionals',
-	'Timber Floor Sanding in Melbourne Gives New Life To Your Floors',
-	'3 Must-Knows Before Floor Sanding Your Melbourne Home',
-	'Wooden Floorboards: How to Clean and Maintain',
-	'The Ins and Outs of Whitewash Flooring',
-	'Repairing and Maintaining Timber Flooring for Melbourne Homes',
-	'Floor Sanding and Polishing: The Boost Your Flooring May Need',
-  	'Timber Flooring Trends From Around the World',
-	'When Should You Sand Back Your Timber Floors?',
-	'Trending in 2019: Oak Flooring in the Kitchen',
-	'Precision Counts Every Step of the Way When Laying Timber Flooring',
-	'How Do I Care For My Wooden Floors?',
-	'Should You Choose a Matte Finish for Your Timber Floors?',
-	'Why Engineered Timber Flooring is Perfect for Australian Homes',
-	'What to Consider Before Staining Your Timber Floors | Kustom Timber',
-	'How to Ensure Your Oak Flooring Melbourne Stands the Test of Time',
-	'5 Mistakes To Avoid With Timber Flooring Installation',
-	'The Benefits of Blackbutt Flooring for Your Home',
-	'The Benefits of Choosing Lighter Coloured Timber Flooring',
-	'How to Maintain Your Engineered Oak Flooring',
-	'Hardwood vs Laminate: Which is Better? | Kustom Timber',
-	'Common Mistakes People Make with Floor Sanding Melbourne',
-	'Preparing for Timber Flooring Installation Melbourne',
-	'Top 5 Timber Flooring Myths',
-	'FAQs About Engineered Oak Flooring: We Answer Your Questions',
-	'Carpet vs Timber Stairs: Which is Right For Your Home?',
-	'Top 7 Home Design Trend Predictions for 2019',
+function ct_blog_posts() { // Insert Blog Post
+    $directory = get_stylesheet_directory() . '/blog_posts_content/';
+    $data = array(
+        array( 'modify' => false, 'title' => 'Tips and Tricks To Make Your Timber Floors Look Like New', 'post_id' => null, 'thumbnail' => 'KINGSLEYLN6-U-1.jpg'),
+        array( 'modify' => false, 'title' => 'Getting The Most Out of Your Melbourne Hardwood Flooring Service', 'post_id' => null, 'thumbnail' => 'installation-services-980x980-1.jpg'),
+        array( 'modify' => false, 'title' => 'What You Need To Know About Melbourne Engineered Timber Flooring', 'post_id' => null, 'thumbnail' => 'Engineered-Timber-Flooring-980x653-1.jpg'),
+        array( 'modify' => false, 'title' => 'Popular Timber Flooring Colours Perfect For Your Home Design', 'post_id' => null, 'thumbnail' => 'Bec_Judd_Buller_home_timber_flooring-1280x854-1.jpg'),
+        array( 'modify' => false, 'title' => 'Why Most Australians Go For Timber Flooring Installations', 'post_id' => null, 'thumbnail' => 'Kustom-Timber-Roseberry-Ave_Simon-Shiff_FINALS_0-15_WEB-JPEG-72DPI-sRGB-1.jpg'),
+        array( 'modify' => false, 'title' => 'Things to Consider Before You Install a Wood Floor', 'post_id' => null, 'thumbnail' => 'Kustom-Timber-Moore-St-Finals_SimonShiff._0-23-980x654-1.jpg'),
+        array( 'modify' => false, 'title' => 'Timber Flooring: Different Species, Different Characteristics', 'post_id' => null, 'thumbnail' => 'Timber-Flooring-melbourne-980x654-1.jpg'),
+        array( 'modify' => false, 'title' => 'The Complete Process of How To Sand Timber Floors', 'post_id' => null, 'thumbnail' => 'floor-sanding-melbourne-1.jpg'),
+        array( 'modify' => false, 'title' => 'A Buyer’s Guide for Choosing The Best Wooden Flooring', 'post_id' => null, 'thumbnail' => 'Wooden-Flooring-980x654-1.jpg'),
+        array( 'modify' => false, 'title' => 'Timber Stairs - Give me the facts', 'post_id' => null, 'thumbnail' => 'Kustom-Timber-26-Danks-St_Simon-Shiff_0-15_-1.jpg'),
+        array( 'modify' => false, 'title' => 'What is Blackbutt Flooring?', 'post_id' => null, 'thumbnail' => 'outback_blackbutt-980x980-1.jpg'),
+        array( 'modify' => false, 'title' => 'Why Choose Oak Flooring?', 'post_id' => null, 'thumbnail' => 'Oak-Flooring-1-980x730-1.jpg'),
+        array( 'modify' => false, 'title' => 'What Can You Expect From Timber Flooring Services Melbourne?', 'post_id' => null, 'thumbnail' => 'timber-flooring-services-melbourne-1280x853-1.jpg'),
+        array( 'modify' => false, 'title' => '4 Signs That You Should Leave Your Floor Sanding to The Professionals', 'post_id' => null, 'thumbnail' => 'floor-sanding-melbourne-1.jpg'),
+        array( 'modify' => false, 'title' => 'Timber Floor Sanding in Melbourne Gives New Life To Your Floors', 'post_id' => null, 'thumbnail' => 'Floor-Sanding-980x653-1.jpg'),
+        array( 'modify' => false, 'title' => '3 Must-Knows Before Floor Sanding Your Melbourne Home', 'post_id' => null, 'thumbnail' => 'floor-sanding-melbourne-980x823-1.jpg'),
+        array( 'modify' => false, 'title' => 'Wooden Floorboards: How to Clean and Maintain', 'post_id' => null, 'thumbnail' => 'Wooden-Floorboards-700x675-1.jpg'),
+        array( 'modify' => false, 'title' => 'The Ins and Outs of Whitewash Flooring', 'post_id' => null, 'thumbnail' => 'Whitewash-Flooring-980x735-1.jpg'),
+        array( 'modify' => false, 'title' => 'Repairing and Maintaining Timber Flooring for Melbourne Homes', 'post_id' => null, 'thumbnail' => 'Timber-Flooring-980x653-1.jpg'),
+        array( 'modify' => false, 'title' => 'Floor Sanding and Polishing: The Boost Your Flooring May Need', 'post_id' => null, 'thumbnail' => 'Floor-Sanding-and-Polishing-980x653-1.jpg'),
+        array( 'modify' => false, 'title' => 'Timber Flooring Trends From Around the World', 'post_id' => null, 'thumbnail' => 'Timber-Flooring-Trends-980x653-1.jpg'),
+        array( 'modify' => false, 'title' => 'When Should You Sand Back Your Timber Floors?', 'post_id' => null, 'thumbnail' => 'Timber-Floors-980x653-1.jpg'),
+        array( 'modify' => false, 'title' => 'Trending in 2019: Oak Flooring in the Kitchen', 'post_id' => null, 'thumbnail' => 'Oak-Flooring-980x653-1.jpg'),
+        array( 'modify' => false, 'title' => 'Precision Counts Every Step of the Way When Laying Timber Flooring', 'post_id' => null, 'thumbnail' => 'Timber-Flooring-melbourne-980x654-1.jpg'),
+        array( 'modify' => false, 'title' => 'How Do I Care For My Wooden Floors?', 'post_id' => null, 'thumbnail' => 'Wooden-Floors-980x653-1.jpg'),
+        array( 'modify' => false, 'title' => 'Should You Choose a Matte Finish for Your Timber Floors?', 'post_id' => null, 'thumbnail' => 'Matte-Finish-980x653-1.jpg'),
+        array( 'modify' => false, 'title' => 'Why Engineered Timber Flooring is Perfect for Australian Homes', 'post_id' => null, 'thumbnail' => 'australian-timber-flooring-1280x854-1.jpg'),
+        array( 'modify' => false, 'title' => 'What to Consider Before Staining Your Timber Floors | Kustom Timber', 'post_id' => null, 'thumbnail' => 'Timber-Floor-Staining-980x653-1.jpg'),
+        array( 'modify' => false, 'title' => 'How to Ensure Your Oak Flooring Melbourne Stands the Test of Time', 'post_id' => null, 'thumbnail' => '35-Albert-Street-Port-Melbourne-004-150x150-1.jpg'),
+        array( 'modify' => false, 'title' => '5 Mistakes To Avoid With Timber Flooring Installation', 'post_id' => null, 'thumbnail' => 'installation.jpg'),
+        array( 'modify' => false, 'title' => 'The Benefits of Blackbutt Flooring for Your Home', 'post_id' => null, 'thumbnail' => 'Blackbutt-Flooring.jpg'),
+        array( 'modify' => false, 'title' => 'The Benefits of Choosing Lighter Coloured Timber Flooring', 'post_id' => null, 'thumbnail' => 'Lighter-Coloured-Timber-Flooring-980x653-1.jpg'),
+        array( 'modify' => false, 'title' => 'How to Maintain Your Engineered Oak Flooring', 'post_id' => null, 'thumbnail' => 'Engineered-Oak-Flooring-980x718-1.jpg'),
+        array( 'modify' => false, 'title' => 'Hardwood vs Laminate: Which is Better? | Kustom Timber', 'post_id' => null, 'thumbnail' => 'laminate-vs-hardwood-1280x854-1.jpg'),
+        array( 'modify' => false, 'title' => 'Common Mistakes People Make with Floor Sanding Melbourne', 'post_id' => null, 'thumbnail' => 'floor-sanding-blog-1280x853-1.jpg'),
+        array( 'modify' => false, 'title' => 'Preparing for Timber Flooring Installation Melbourne', 'post_id' => null, 'thumbnail' => 'timber-flooring-installation-melbourne-960x640-1.jpg'),
+        array( 'modify' => false, 'title' => 'Top 5 Timber Flooring Myths', 'post_id' => null, 'thumbnail' => 'Timber-Flooring-Myths-980x686-1.jpg'),
+        array( 'modify' => false, 'title' => 'FAQs About Engineered Oak Flooring: We Answer Your Questions', 'post_id' => null, 'thumbnail' => 'engineered-oak-flooring-1.jpg'),
+        array( 'modify' => false, 'title' => 'Carpet vs Timber Stairs: Which is Right For Your Home?', 'post_id' => null, 'thumbnail' => 'timber-stairs-melbourne-1.jpg'),
+        array( 'modify' => false, 'title' => 'Top 7 Home Design Trend Predictions for 2019', 'post_id' => null, 'thumbnail' => 'timber-flooring-melbourne-1.jpg'),
     );
-    for ($i = 0; $i < 40; $i++) {
-
+    $insert_blog_post = false;
+    // $post_id = 18376;
+    foreach($data as $index => $value) {
+        if($index >= 40) { break; }
         $data = array(
-            'post_title'  => $titles[$i],
+            'post_title'  => $value['title'],
             'post_status' => 'publish',
             'post_type'   => 'post',
         );
-
-        $post_id = wp_insert_post($data);
-        wp_set_post_terms($post_id, 161, 'category');
-
-        $file_content = file_get_contents(get_stylesheet_directory() . '/blog_posts_content/' . $i . '.txt');
-
-        $sections_field_key = 'sections';
-
+        if($insert_blog_post !== false) { 
+            $post_id = wp_insert_post($data); 
+            $value[$index]['post_id'] = $post_id;
+            wp_set_post_terms($post_id, 161, 'category');
+        }
+        $content = '';
+        // if($value['modify'] == true) {
+            $content = file_get_contents($directory . $index . '/content.txt');
+        // }
         $acf_field_values = array(
             array( // Blog Child Hero - Component UI
                 'acf_fc_layout' => 'blog_child_hero', 
@@ -107,12 +88,12 @@ function add_blog_posts() { // Insert Blog Post
                     'data_fields' => array(
                         'information' => array(
                             array(
-                                'text_summary' => $file_content,
+                                'text_summary' => $content,
                             ),
-                            // array(
-                            //     'text_summary' => $file_content,
-                            // ),
                         ),
+                    ),
+                    'layout_select' => array(
+                        'layout_select' => 'layout2',
                     ),
                 ),
             ),
@@ -121,40 +102,12 @@ function add_blog_posts() { // Insert Blog Post
                 'blog_child_related_articles' => array(),
             ),
         );
-
-        update_field($sections_field_key, $acf_field_values, $post_id);
-
+        if($value['modify'] == true) { update_field('sections', $acf_field_values, $post_id); };
+        $featured_image_url = get_site_url() . '/wp-content/uploads/2023/06/' . $value['thumbnail'];
+        if(!empty($featured_image_url)) {
+            $image_id = attachment_url_to_postid($featured_image_url);
+            if($insert_blog_post !== false) { set_post_thumbnail($post_id, $image_id); }
+        }
     }
 }
-
-function update_blog_posts() { // Update Blog Post
-
-        $post_id = 15797;
-
-        $file_content = file_get_contents(get_stylesheet_directory() . '/blog_posts_content/' . '39' . '.txt');
-        // $file_content = file_get_contents(get_stylesheet_directory() . '/blog_posts_content/modify/modify.txt');
-
-        $sections_field_key = 'sections';
-
-        $acf_field_values = array(
-            array( // Blog Child Gallery - Component UI
-                'acf_fc_layout' => 'blog_child_gallery', 
-                'blog_child_gallery' => array(
-                    'data_fields' => array(
-                        'information' => array(
-                            array(
-                                'text_summary' => $file_content,
-                            ),
-                        ),
-                    ),
-                ),
-            ),
-        );
-
-        update_field($sections_field_key, $acf_field_values, $post_id);
-
-}
-
-
-if(isset($_GET['fred'])) { add_action('init', 'add_blog_posts'); }
-if(isset($_GET['dame'])) { add_action('init', 'update_blog_posts'); }
+if(isset($_GET['fred'])) { add_action('init', 'ct_blog_posts'); }
