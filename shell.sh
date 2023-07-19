03aeb570cd3b45e083c4e58a551c2d93189a418c
fsanterva <fsanterva@protonmail.com>
2023-07-19 11:39:35 +0800

    save

--- shell.sh
+++ shell.sh
@@ -1,7 +1,15 @@
-flag=false
-while [ "$flag" = false ]; do
-  export LANG=en_US.UTF-8
-  rsync -avz --update --delete --progress kustomtimber@kustomtimber.ddev.com.au:/home/kustomtimber/public_html/wp-content/themes/dilate-framework/* /mnt/c/wamp64/www/devs/dilate/wp/kustomtimber/wp-content/themes/dilate-framework/
-  git status .
-flag=true
-done
+# flag=false
+# while [ "$flag" = false ]; do
+#   export LANG=en_US.UTF-8
+#   rsync -avz --update --delete --progress kustomtimber@kustomtimber.ddev.com.au:/home/kustomtimber/public_html/wp-content/themes/dilate-framework/* /mnt/c/wamp64/www/devs/dilate/wp/kustomtimber/wp-content/themes/dilate-framework/
+#   git status .
+# flag=true
+# done
+
+# flag=true
+# while [ "$flag" = true ]; do
+#   export LANG=en_US.UTF-8
+  rsync -avz --update --delete --progress kustomtimber@kustomtimber.ddev.com.au:/home/kustomtimber/public_html/wp-content/plugins/* /mnt/c/wamp64/www/devs/dilate/wp/kustomtimber/wp-content/plugins/
+#   git status .
+# flag=false
+# done
