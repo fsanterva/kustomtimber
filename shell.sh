a80f773332aec819c0adeca5c35ffb0d2b6eb997
fred <codepen24@protonmail.com>
2023-07-24 13:21:50 +0800

    save

--- shell.sh
+++ shell.sh
@@ -1,15 +1,13 @@
-# flag=false
-# while [ "$flag" = false ]; do
-#   export LANG=en_US.UTF-8
-#   rsync -avz --update --delete --progress kustomtimber@kustomtimber.ddev.com.au:/home/kustomtimber/public_html/wp-content/themes/dilate-framework/* /mnt/c/wamp64/www/devs/dilate/wp/kustomtimber/wp-content/themes/dilate-framework/
-#   git status .
-# flag=true
-# done
+flag=true
+while [ "$flag" = true ]; do
+  export LANG=en_US.UTF-8
+  rsync -avz --update --delete --progress kustomtimber@kustomtimber.ddev.com.au:/home/kustomtimber/public_html/wp-content/themes/dilate-framework/ /mnt/c/wamp64/www/devs/dilate/wp/kustomtimber/public_html/wp-content/themes/dilate-framework/
+  git status .
+flag=false
+done
 
-# flag=true
-# while [ "$flag" = true ]; do
-#   export LANG=en_US.UTF-8
-  rsync -avz --update --delete --progress kustomtimber@kustomtimber.ddev.com.au:/home/kustomtimber/public_html/wp-content/plugins/* /mnt/c/wamp64/www/devs/dilate/wp/kustomtimber/wp-content/plugins/
-#   git status .
-# flag=false
-# done
+# rsync -avz --update --delete --progress
+# kustomtimber@kustomtimber.ddev.com.au:/home/kustomtimber/public_html/wp-content/plugins/
+# kustomtimber@kustomtimber.ddev.com.au:/home/kustomtimber/public_html/wp-content/themes/dilate-framework/
+# /mnt/c/wamp64/www/devs/dilate/wp/kustomtimber/public_html/wp-content/plugins/
+# /mnt/c/wamp64/www/devs/dilate/wp/kustomtimber/public_html/wp-content/themes/dilate-framework/
